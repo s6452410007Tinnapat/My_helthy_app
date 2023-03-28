617ffac7b6dec2c463f029fff8cebc645cda0028
s6452410007Tinnapat <s6452410007@sau.ac.th>
2023-03-28 13:46:10 +0700

    Code V.1 by Pon : create project

---
+++ lib/views/healthy_splashscreen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HealthySplashScreenUI extends StatefulWidget {
+  const HealthySplashScreenUI({super.key});
+
+  @override
+  State<HealthySplashScreenUI> createState() => _HealthySplashScreenUIState();
+}
+
+class _HealthySplashScreenUIState extends State<HealthySplashScreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
